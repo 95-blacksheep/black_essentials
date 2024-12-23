b4c1e7cf6e2d7fbf10d769d53712a501ba0f5e70
Black <95blacksheep04@gmail.com>
2024-12-23 07:58:03 -0800

    PostAuthor, PostItem, Posts, Home pages with styles update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Posts from "../components/Posts";
 
 const Home = () => {
   return (
     <div>
-     <h2>home</h2>
+      <Posts />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
