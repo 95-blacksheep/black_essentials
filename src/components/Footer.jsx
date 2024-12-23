6536c09d7b9e29f31298168a37173db5106158f1
Black <95blacksheep04@gmail.com>
2024-12-22 21:09:14 -0800

    Header, footer && 404  designs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from '../images/logo.png'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+   <footer>
+    <center>
+    <div className="footer_logo">
+        <Link to="/" className="footer_image">
+          <img src={Logo} alt="Black Essentials Logo" />
+        </Link>
+
+        <div className="footer_copyright">
+          <small>All Rights Reserved &copy; Copyright 2024, Black Essentials Inc. </small>
+        </div>
+      </div>
+    </center>
+   </footer>
   )
 }
 
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      404 Page
-    </div>
-  )
-}
+    <section className="error_page">
+      <div className="error_page_text">
+        <center>
+        <p>Error <span> 404</span></p>
+        <p>It seems the page you're looking for does not exist on Black <span>Essentials.</span></p>
+        </center>
+      </div>
 
-export default ErrorPage
+
+        <div class="bar">
+          <div class="ball"></div>
+        </div>
+
+
+        <div className="error_page_redirect">
+          <p>Go back. <span><Link to="/">Home</Link></span></p>
+        </div>
+    </section>
+  );
+};
+
+export default ErrorPage;
