666931ef124445f3f144f8107d45844b8c8680bd
Black <95blacksheep04@gmail.com>
2024-12-23 21:00:48 -0800

    error and success alert, register, and login page created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    
+    <section className="register">
+<ErrorMessage />
+
+
+      <div className="form_container">
+        <h2>Sign In</h2>
+
+        <form className="register_form">
+
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+
+          <button type="submit" className="btn_primary">
+            <h4>Signin</h4>
+          </button>
+        </form>
+
+        <small>
+          Don't have an account? <Link to="/register">Sign Up Here</Link>
+        </small>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
 
-export default Register
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    
+    <section className="register">
+<ErrorMessage />
+
+
+      <div className="form_container">
+        <h2>Sign Up</h2>
+
+        <form className="register_form">
+         
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            value={userData.name}
+            onChange={changeInputHandler}
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirm_password"
+            value={userData.confirm_password}
+            onChange={changeInputHandler}
+          />
+
+          <button type="submit" className="btn_primary">
+            <h4>Submit</h4>
+          </button>
+        </form>
+
+        <small>
+          Already have an account? <Link to="/login">Sign In Here</Link>
+        </small>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
