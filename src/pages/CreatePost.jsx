99bebc8d58e1f5d3e2c7f7e72de2cb80552553c9
95-Blacksheep <95blacksheep04@gmail.com>
2024-12-27 10:53:05 -0800

    create post module complete with react-quil import

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
+import ReactQuill from 'react-quill'; /*  for the text area  */
+import 'react-quill/dist/quill.snow.css'
+
 import ErrorMessage from "./ErrorMessage";
+
+
 
 
 
@@ -13,21 +18,21 @@ const CreatePost = () => {
 
   const modules = {
     toolbar: [
-      [{'header': [1, 2, false]}],
+      [{'header': [1, 2, 3, 4, 5, 6, false]}],
       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'} , {'indent': '+1'}]
-      ['link', 'image'], 
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
       ['clean']
     ]
-  }
-
-
+  };
+  
   const formats = [
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link',  'image'
-  ]
+    'link', 'image'
+  ];
+  
 
 
 
@@ -35,19 +40,25 @@ const CreatePost = () => {
 
 
   return (
-    <section className="register">
+    <section className="register" id='create_post'>
       <ErrorMessage />
       <div className="form_container">
 
-      <h2>Sign Up</h2>
+      <h2>New Post</h2>
 
-        <div className="register_form">
+        <div className="register_form" id='editor'>
           <input type="text" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)}  autoFocus/>
           <select name="category" value={category} onChange={e =>  setCategory(e.target.value)}>
             {
-
+              Post_Categories.map(cat => <option key={cat}>{cat}</option> )
             }
           </select>
+
+          <ReactQuill modules={modules}  formats={formats} value={desc} onChange={setDesc}/>
+
+          <input type="file" onChange={e => setThumbnail(e.target.files(0))} accept='png, jpg, jpeg' />
+
+          <button type='submit' className='btn_primary' >Post</button>
         </div>
       </div>
     </section>
